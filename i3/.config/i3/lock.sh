5e5957ea96031f557fc031c79b97c83b2571b1e4
flokoe <koehler.florian@gmx.de>
2017-08-10 17:43:41 +0200

    Reduced code. added dynamic monitor blur

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -4,20 +4,19 @@ icon="$HOME/.config/i3/lock.png"
 lockImage="/tmp/lockImage.png"
 n=1
 x=0
+displays="$(xrandr |grep -c -w connected)"
 
 scrot "$lockImage"
 
-while [ "$n" -le 3 ]
+while [ "$n" -le $displays ]
 do
-  convert -extract 1920x1200+"$x"+0 "$lockImage" /tmp/lockImage"$n".png
-  convert /tmp/lockImage"$n".png -scale 25% -blur 0x15 -scale 400% /tmp/lockImage"$n".png
+  mogrify -region 1920x1200+"$x"+0 -scale 25% -blur 0x15 -scale 400% "$lockImage"
   ((x+=1920))
   ((n++))
 done
 
-convert /tmp/lockImage1.png /tmp/lockImage2.png /tmp/lockImage3.png +append "$lockImage"
 convert "$lockImage" "$icon" -gravity center -composite -matte "$lockImage"
 
-#i3lock -i "$lockImage"
+i3lock -i "$lockImage"
 
-rm /tmp/lockImage*.png
+rm "$lockImage"
