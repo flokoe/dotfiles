032abee7a10b749b8b06ce4ccb5550d955a021a4
flokoe <koehler.florian@gmx.de>
2019-09-20 23:14:29 +0200

    Enhance ex to extract multiple files and more formats

--- bash/_inc/functions.bash
+++ bash/_inc/functions.bash
@@ -45,22 +45,35 @@ uniqlines () {
 
 # Magically extract any archive regardless if its file extension
 ex () {
-  if [ -f $1 ] ; then
-    case $1 in
-      *.tar.bz2)   tar xjf $1   ;;
-      *.tar.gz)    tar xzf $1   ;;
-      *.bz2)       bunzip2 $1   ;;
-      *.rar)       unrar x $1   ;;
-      *.gz)        gunzip $1    ;;
-      *.tar)       tar xf $1    ;;
-      *.tbz2)      tar xjf $1   ;;
-      *.tgz)       tar xzf $1   ;;
-      *.zip)       unzip $1     ;;
-      *.Z)         uncompress $1;;
-      *.7z)        7z x $1      ;;
-      *)           echo "'$1' cannot be extracted via ex()" ;;
-    esac
-  else
-    echo "'$1' is not a valid file"
-  fi
+  # for each argument (file) try to extract
+  while [[ $# > 0 ]] ; do
+    # Check if file exists
+    if [[ -f $1 ]] ; then
+      case "$1" in
+        *.tar.bz2|*.tbz|*.tbz2) tar "x${verbose}jf" "$1" ;;
+        *.tar.gz|*.tgz) tar "x${verbose}zf" "$1" ;;
+        *.tar.xz) xz --decompress "$1"; set -- "$@" "${1:0:-3}" ;;
+        *.tar.Z) uncompress "$1"; set -- "$@" "${1:0:-2}" ;;
+        *.bz2) bunzip2 "$1" ;;
+        *.deb) dpkg-deb -x${verbose} "$1" "${1:0:-4}" ;;
+        *.pax.gz) gunzip "$1"; set -- "$@" "${1:0:-3}" ;;
+        *.gz) gunzip "$1" ;;
+        *.pax) pax -r -f "$1" ;;
+        *.pkg) pkgutil --expand "$1" "${1:0:-4}" ;;
+        *.rar) unrar x "$1" ;;
+        *.rpm) rpm2cpio "$1" | cpio -idm${verbose} ;;
+        *.tar) tar "x${verbose}f" "$1" ;;
+        *.txz) mv "$1" "${1:0:-4}.tar.xz"; set -- "$@" "${1:0:-4}.tar.xz" ;;
+        *.xz) xz --decompress "$1" ;;
+        *.zip|*.war|*.jar) unzip "$1" ;;
+        *.Z) uncompress "$1" ;;
+        *.7z) 7za x "$1" ;;
+        *) echo "'$1' cannot be extracted via ex" ;;
+      esac
+    else
+      echo "ex: '$1' is not a valid file"
+    fi
+
+    shift
+  done
 }
