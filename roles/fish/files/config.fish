fc672ac797e7f9b927ede0ca6dee3da39fe576bf
flokoe <koehler.florian@gmx.de>
2022-06-21 22:21:50 +0200

    Disable greeting on startup

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,5 +1,9 @@
 # Commands to run in interactive sessions can go here
 if status is-interactive
+    # Disable greeting on startup
+    set -U fish_greeting
+
+    # Set important variables
     set -gx EDITOR vim
 
     starship init fish | source
