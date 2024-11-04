0bbcbea3501584889f24fbb389e48fd916a762a5
Florian Köhler <git@flokoe.de>
2024-11-04 20:44:43 +0100

    Remove checks to improve run time
    I can guarantee, that the requirements are met.

--- vault-bitwarden-client.sh
+++ vault-bitwarden-client.sh
@@ -16,21 +16,4 @@ if [[ -z $vault_id ]]; then
   exit 2
 fi
 
-if ! which bw &> /dev/null; then
-  echo "Bitwarden CLI is not installed"
-  exit 3
-fi
-
-if ! which jq &> /dev/null; then
-  echo "jq is not installed"
-  exit 4
-fi
-
-bw_status="$(bw status | jq -r .status)"
-
-if [[ $bw_status != "unlocked" ]]; then
-  echo "Bitwarden is not unlocked"
-  exit 5
-fi
-
 bw get item "$vault_id" | jq -r .login.password
