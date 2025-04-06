75706cc5665446b174b6cae61ebbcc8f3c9a1d21
Florian Köhler <git@flokoe.de>
2025-04-06 13:51:50 +0200

    Ensure neovim flatpak is absent

--- roles/fish/files/functions/nvim.fish
+++
@@ -1,7 +0,0 @@
-function nvim --wraps=io.neovim.nvim --description 'alias nvim=io.neovim.nvim'
-  io.neovim.nvim $argv
-end
-
-function vim --wraps=io.neovim.nvim --description 'alias vim=io.neovim.nvim'
-  io.neovim.nvim $argv
-end
